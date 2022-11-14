5d8597f506b13cb689bacf5b3c5fb2bc9e6a6e4b
dustin-ward <wardddustin@gmail.com>
2022-11-14 11:40:28 -0700

    Camera working

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,26 @@
-
-document.addEventListener('deviceready', onDeviceReady, false);
+document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
+  // Cordova is now initialized. Have fun!
 
+  var options = {
+    quality: 80,
+    destinationType: Camera.DestinationType.FILE_URI,
+  };
+
+  $("#takePhoto").on("click", () => {
+    navigator.camera.getPicture(onSuccess, onFail, options);
+  });
+
+  function onSuccess(imageData) {
+    resolveLocalFileSystemURL(imageData, function (fileEntry) {
+        var imgURL = fileEntry.toURL()
+        var img = new Image();
+        img.src = imgURL;
+        $("#imageContainer").append(img);
+    }, onFail);
+  }
+  function onFail(message) {
+    console.log("ERROR", message);
+  }
 }
