ce0fa7abe2307507afbcc772c0c60372f5c5bb68
dustin-ward <wardddustin@gmail.com>
2022-11-16 11:46:03 -0700

    Finished

--- www/js/index.js
+++ www/js/index.js
@@ -3,15 +3,18 @@ document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
   // Cordova is now initialized. Have fun!
 
+  // Camera Options
   var options = {
     quality: 80,
     destinationType: Camera.DestinationType.FILE_URI,
   };
 
+  // Photo button click event handler
   $("#takePhoto").on("click", () => {
     navigator.camera.getPicture(onSuccess, onFail, options);
   });
 
+  // On successful image
   function onSuccess(imageData) {
     resolveLocalFileSystemURL(imageData, function (fileEntry) {
         var imgURL = fileEntry.toURL()
@@ -20,7 +23,10 @@ function onDeviceReady() {
         $("#imageContainer").append(img);
     }, onFail);
   }
+
+  // On failure to take image
   function onFail(message) {
     console.log("ERROR", message);
+    alert("Error: Image not taken")
   }
 }
